2c3f715f506fc6c7041e2d0a18ce494ca11d720c
CraigJensen <cmjensen82@gmail.com>
2022-03-07 16:24:27 -0600

    user routes working

--- server.js
+++ server.js
@@ -9,4 +9,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(require('./routes'));
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/NoSQL-API',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// Use this to log mongo queries being executed
+mongoose.set('debug', true);
+
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
