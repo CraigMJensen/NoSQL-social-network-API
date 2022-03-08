bb426dd5dde0ba61352d5acdb289cfabe633cf35
CraigJensen <cmjensen82@gmail.com>
2022-03-08 16:18:38 -0600

    added friends in user-controllers and user-routes, working

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -35,6 +35,22 @@ const userController = {
       .catch((err) => res.json(err));
   },
 
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $push: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
   updateUser({ params, body }, res) {
     User.findOneAndUpdate({ _id: params.id }, body, { new: true })
       .select('-__v')
@@ -57,6 +73,15 @@ const userController = {
         }
         res.jsonn(dbUserData);
       })
+      .catch((err) => res.json(err));
+  },
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.json(err));
   },
 };
