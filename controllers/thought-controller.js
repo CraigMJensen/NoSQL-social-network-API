fb7f9d60442ead721d36b7a3c028885319b804d4
CraigJensen <cmjensen82@gmail.com>
2022-03-10 15:32:49 -0600

    can GET one thought and GET all thoughts

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -3,11 +3,28 @@ const { Thought, User } = require('../models');
 const thoughtController = {
   getAllThoughts(req, res) {
     Thought.find({})
+      .select('-__v')
       .sort({ _id: -1 })
       .then((dbThoughtData) => res.json(dbThoughtData))
       .catch((err) => {
         console.log(err);
         res.sendStatus(400);
+      });
+  },
+
+  getOneThought({ params }, res) {
+    Thought.findOne({ _id: params.id })
+
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought with that id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
       });
   },
 
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 const {
   getAllThoughts,
+  getOneThought,
   addThought,
 } = require('../../controllers/thought-controller');
 
 router.route('/').get(getAllThoughts);
 
 router.route('/:userId').post(addThought);
+
+router.route('/:id').get(getOneThought);
 
 module.exports = router;
